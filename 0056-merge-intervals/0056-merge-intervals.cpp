820b913ae99f45d320ca53985af2f8396b5bd165
Bani Gupta <banigupta2212@gmail.com>
2026-07-11 13:30:39 +0530

    Time: 3 ms (88.99%), Space: 23.9 MB (30.83%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,22 +6,14 @@ public:
         sort(intervals.begin(),intervals.end());
         for(int i = 0;i<n;i++)
         {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.empty() && end<=ans.back()[1]) continue;
-
-            for(int j = i+1;j<n;j++)
+            if(ans.empty() || intervals[i][0] > ans.back()[1])
             {
-                if(intervals[j][0]<=end)
-                {
-                    end = max(end,intervals[j][1]);
-                }
-                else
-                {
-                    break;
-                }
+                ans.push_back(intervals[i]);
             }
-            ans.push_back({start,end});
+            else
+            {
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+            }
         }
         return ans;
         
