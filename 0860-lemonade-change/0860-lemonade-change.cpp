016b6dd17962b49e4233361b1f31d88d1edf21f7
Bani Gupta <banigupta2212@gmail.com>
2026-08-12 18:33:09 +0530

    Time: 0 ms (100%), Space: 87.4 MB (11.37%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+       int count5 = 0;
+       int count10 = 0;
+       int count20 = 0;
+       int n = bills.size();
+       for(int i = 0;i< n;i++)
+       {
+        if(bills[i]==5) count5++;
+        else if(bills[i]==10)
+        {
+            if(count5== 0) return false;
+            else
+            {
+                count5--;
+                count10++;
+            }
+        }
+        else
+        {
+            if(count5 == 0) return false;
+            else if(count10 == 0) 
+            {
+                if(count5>=3) count5 = count5 - 3;
+                else return false;
+            }
+            else
+            {
+                count5--;
+                count10--;
+                count20++;
+            }
+        }
+       }
+       return true;
+
+    }
+};
