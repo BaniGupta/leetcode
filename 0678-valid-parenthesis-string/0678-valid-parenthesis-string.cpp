c3306465f7f7e3aeed55ca243caf184799f8df96
Bani Gupta <banigupta2212@gmail.com>
2026-08-14 07:55:17 +0530

    Time: 0 ms (100%), Space: 8.2 MB (48.29%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int mini = 0;
+        int maxi = 0;
+        for(int i = 0;i < s.size();i++)
+        {
+            if(s[i]=='(')
+            {
+                mini++;
+                maxi++;
+            }
+            else if(s[i]==')')
+            {
+                mini--;
+                maxi--;
+            }
+            else
+            {
+                mini--;
+                maxi++;
+            }
+            if(maxi < 0) return false;
+            if(mini < 0) mini = 0;
+        }
+        return mini == 0;
+        
+        
+    }
+};
